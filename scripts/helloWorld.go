b5c5233113f32df47da7b9d7770e758154eb6665
Aditya Singh <singh.aditya8499@gmail.com>
2022-09-10 11:11:38 -0400

    Add new import functions

---
+++ scripts/helloWorld.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"math/rand"
+)
+
+func main() {
+	fmt.Println("Hello world from Golang!")
+	fmt.Println("Math operation: ", math.Sqrt(10))
+	fmt.Println("Random integer: ", rand.Intn(100))
+}
